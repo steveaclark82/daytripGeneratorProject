b450eb0642c4d9f919313fed32e4be7623c621c9
steveaclark82 <steve.a.clark48@gmail.com>
2021-07-05 10:29:23 -0500

    init

---
+++ day trip.js
@@ -0,0 +1,52 @@
+"user strict"
+
+
+let chosenDestination = ["miami", "orlando", "clearwater", "sarasota"];
+
+function getRandomIndex(items) {
+    return Math.floor(Math.random() * items.length);
+}
+for (let i = 0; i < 1; i++) {
+    let removedItem = chosenDestination.splice(getRandomIndex(chosenDestination), 1);
+    console.log(removedItem);
+}
+
+let chosenRestaurant = ["mexican", "italian", "carribean", "bbq", "seafood", "cajun"];
+
+function getRandomIndex(items) {
+    return Math.floor(Math.random() * items.length);
+}
+for (let i = 0; i < 1; i++) {
+    let removedItem = chosenRestaurant.splice(getRandomIndex(chosenRestaurant), 1);
+    console.log(removedItem);
+}
+
+let chosenTransport = ["plane", "train", "car", "taxicab"];
+
+function getRandomIndex(items) {
+    return Math.floor(Math.random() * items.length);
+}
+for (let i = 0; i < 1; i++) {
+    let removedItem = chosenTransport.splice(getRandomIndex(chosenTransport), 1);
+    console.log(removedItem);
+}
+
+let chosenEntertainment = ["movie", "music", "dance", "sport", "boating", "skydiving"];
+
+function getRandomIndex(items) {
+    return Math.floor(Math.random() * items.length);
+}
+for (let i = 0; i < 1; i++) {
+    let removedItem = chosenEntertainment.splice(getRandomIndex(chosenEntertainment), 1);
+    console.log(removedItem);
+}
+
+let newChosenDayTrip = ("chosenDestination", "chosenEntertainment", "chosenRestaurant", "chosenTransport");
+
+function getRandomIndex(items) {
+    return Math.floor(Math.random() * items.length);
+}
+for( let i = 0; i < 1; i++) {
+    let removedItem = newChosenDayTrip;
+    console.log(removedItem);
+}
