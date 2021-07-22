11a81df3783b178a8466ffe867e2d55ee3b06d1d
Steve Clark <steve.a.clark48@gmail.com>
2021-07-22 12:20:27 -0500

    removed mode from line 12

--- day trip.js
+++ day trip.js
@@ -9,7 +9,7 @@ let chosenEntertainment = ["movie", "music", "dance", "sport", "boating", "skydi
 // Randomly selected trip details
 let randomDestination = getTripItem(chosenDestination);
 let randomRestaurant = getTripItem(chosenRestaurant);
-let randomTransportMode = getTripItem(chosenTransportMode);
+let randomTransport = getTripItem(chosenTransport);
 let randomEntertainment = getTripItem(chosenEntertainment);
 
 
